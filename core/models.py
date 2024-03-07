9209f9cacf9d0e3739913ac72f769a0eeb53961e
Bishnu <thapabishnu20@gmail.com>
2024-03-07 21:43:10 +0545

    add product and category model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,145 @@
 from django.db import models
-
+from django.utils.html import mark_safe
+from ckeditor_uploader.fields import RichTextUploadingField
+from taggit.managers import TaggableManager
 # Create your models here.
+
+
+STATUS_CHOICE = (
+    ("process", "Pocessing"),
+    ("shipped", "Shipped"),
+    ("delivered", "Delivered")
+)
+
+UNIT = (
+    ("kg", "Kilogram"),
+    ("pieces", "Pieces")
+)
+
+STATUS = (
+    ("draft", "Draft"),
+    ("disabled", "Disabled"),
+    ("rejected", "Rejected"),
+    ("in_review", "In Review"),
+    ("published", "Published")
+)
+RATING = (
+    (1, "⭐"),
+    (2, "⭐⭐"),
+    (3, "⭐⭐⭐"),
+    (4, "⭐⭐⭐⭐"),
+    (5, "⭐⭐⭐⭐⭐")
+)
+
+
+class Banner(models.Model):
+    image = models.ImageField(upload_to='slider',
+                              help_text='1920*1280 px')
+    heading = models.CharField(max_length=255, null=True, blank=True)
+    sub_heading = models.TextField(null=True, blank=True)
+    link = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.heading
+
+    class Meta:
+        verbose_name = 'Slider'
+        verbose_name_plural = 'Slider'
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
+    image = models.ImageField(upload_to='category')
+    icon = models.ImageField(upload_to='category')
+    description = RichTextUploadingField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def category_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))
+
+    def __str__(self):
+        return self.title
+
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
+    image = models.ImageField(upload_to='category')
+    icon = models.ImageField(upload_to='category')
+    description = RichTextUploadingField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        verbose_name = 'Sub Category'
+        verbose_name_plural = 'Sub Categories'
+
+    def sub_category_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))
+
+    def __str__(self):
+        return self.title
+
+
+class Brand(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
+    image = models.ImageField(upload_to='category')
+    icon = models.ImageField(upload_to='category')
+    description = RichTextUploadingField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        verbose_name = 'Brand'
+        verbose_name_plural = 'Brands'
+
+    def brand_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, related_name='category')
+    sub_category = models.ForeignKey(
+        SubCategory, on_delete=models.SET_NULL, null=True, related_name='subcategory')
+    brand = models.ForeignKey(
+        Brand, on_delete=models.SET_NULL, null=True, related_name='brand')
+    image = models.ImageField(upload_to='products')
+    unit = models.CharField(choices=UNIT, max_length=10, default='pieces')
+    tags = TaggableManager(blank=True)
+    description = RichTextUploadingField(null=True, blank=True)
+    exchangeable = models.BooleanField(default=False)
+    refundable = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def product_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))
+
+    def __str__(self):
+        return self.title
+
+    # def get_percentage(self):
+    #     new_price = ((self.old_price-self.price)*100)/self.old_price
+    #     return new_price
